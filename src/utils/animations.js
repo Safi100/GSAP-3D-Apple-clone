93585c01f17a322f7ecd6a2d258ac5d5469b2052
safi100 <safinafi12@gmail.com>
2024-07-05 01:09:21 +0300

    Create animation function to prevent repeat of code

--- src/utils/animations.js
+++ src/utils/animations.js
@@ -1,3 +1,19 @@
+import gsap from "gsap";
+import ScrollTrigger from "gsap/all";
+gsap.registerPlugin(ScrollTrigger);
+
+export const animate_with_Gsap = (target, animationProbs, scrollProps) => {
+    gsap.to(target, {
+        ...animationProbs,
+        scrollTrigger: {
+            trigger: target,
+            toggleActions: 'restart reverse restart reverse',
+            start: "top 85%",
+            ...scrollProps
+        },
+    })
+}
+
 export const animation_Gsap_timeline = (timeLine, rotationRef, rotationState, firstTarget, secondTarget, animationProps) => {
     timeLine.to(rotationRef.current.rotation, {
         y: rotationState,
